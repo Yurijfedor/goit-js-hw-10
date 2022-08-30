55c1f5a024ce8dc79c53ae65d13e712ab5701d21
Yurijfedor <99280782+Yurijfedor@users.noreply.github.com>
2022-08-30 02:53:35 +0000

    Deploying to gh-pages from @ Yurijfedor/goit-js-hw-10@f599c4a5f6571b3a4097ccd8b05166c9545194ea 🚀

--- index.9d21c8f5.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v2/name/name").then((function(n){return n.json()})).then(console.log).catch((function(n){return console.log(n)}));
-//# sourceMappingURL=index.9d21c8f5.js.map
--- index.bdeeac19.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v2/name/name").then((e=>e.json())).then(console.log).catch((e=>console.log(e)));
-//# sourceMappingURL=index.bdeeac19.js.map
